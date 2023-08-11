051206ba17c0e38ebe31e8bddba77502ab64acb1
SANJAIKUMAR <skasanjai@gmail.com>
2023-08-11 21:56:12 +0530

    driver

--- lib/pages/BookingProcess/Ride/DriverDetails.dart
+++ lib/pages/BookingProcess/Ride/DriverDetails.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
+import 'package:line_icons/line_icons.dart';
 
 class DriverDetails extends StatefulWidget {
   const DriverDetails({super.key});
@@ -28,52 +30,272 @@ class _DriverDetailsState extends State<DriverDetails> {
           backgroundColor: Colors.transparent,
           elevation: 0,
         ),
-        body: Stack(children: [
-          Column(
-            children: [
-              Expanded(
-                child: SingleChildScrollView(
-                  clipBehavior: Clip.none,
-                  scrollDirection: Axis.vertical,
-                  child: Column(
-                    children: [Image.asset('assets/map.png')],
-                  ),
+        body: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                clipBehavior: Clip.none,
+                scrollDirection: Axis.vertical,
+                child: Column(
+                  children: [Image.asset('assets/map.png')],
                 ),
               ),
-              Stack(
-                  fit:StackFit.passthrough,
-                  children:<Widget> [
-                SizedBox(
-                    width: MediaQuery.of(context).size.width,
-                    height: 550,
-                    child: Material(
-                      elevation: 5,
-                      borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(20),
-                          topLeft: Radius.circular(20)),
-                      color: Colors.white,
-                      child: Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Column(),
-                      ),
-                    )),
-                Positioned(
-                  height: -30,
-                  child: CircleAvatar(
-                    radius: 50,
-                    backgroundColor: Colors.black,
-                    child: Icon(
-                      Icons.person,
-                      color: Color.fromRGBO(255, 51, 51, 0.2),
-                      size: 45,
-                    ),
-                  ),
-                ),
-              ]
+            ),
+            Stack(
+              clipBehavior: Clip.none,
+                children:<Widget> [
+              SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 500,
+                  child: Material(
+                    elevation: 5,
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20),
+                        topLeft: Radius.circular(20)),
+                    color: Colors.white,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 30,
+                            child: Divider(
+                              thickness: 3,
+                            ),
+                          ),
+                          SizedBox(height: 30,),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Column(
+                                children: [
+                                  Material(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: Colors.grey,
+                                    child: Padding(
+                                      padding: EdgeInsets.only(left: 5,right: 5),
+                                      child: Text('TN 39 BR 1446',style: TextStyle(fontFamily: 'Arimo',color: Colors.white,fontWeight: FontWeight.bold),),
+                                    ),
+                                  ),
+                                  SizedBox(height: 5,),
+                                  Text('Volkswagen Jeta',style: TextStyle(
+                                      fontFamily: 'Arimo',
+                                      color: Colors.black54,
+                                      fontSize: 14
+                                  ))
+                                ],
+                              )
+                            ],
+                          ),
+                          SizedBox(height: 40,),
+                              historyBox(),
+                          SizedBox(height: 40,),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 30,right: 30),
+                            child: Material(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Color.fromRGBO(255, 51, 51, 0.9),
+                              child: MaterialButton(
+                                padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+                                minWidth: MediaQuery.of(context).size.width,
+                                onPressed: () async {
+                                  Navigator.push(context,
+                                      MaterialPageRoute(builder: (context) => DriverDetails()));
+                                },
+                                child:  Text(
+                                  "Make payment",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontSize: 17,
+                                      fontFamily: 'Arimo',
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 20,),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              PhysicalModel(
+                                elevation:2,
+                                shape: BoxShape.circle,
+                                color: Colors.transparent,
+                                child:
+                              CircleAvatar(
+                                radius: 30,
+                                backgroundColor:
+                                Colors.white,
+                                child: InkWell(
+                                  onTap: () async {
+                                    await FlutterPhoneDirectCaller.callNumber('7373994102');
+                                  },
+                                  child:Icon(
+                                  LineIcons.phoneVolume,
+                                  color:
+                                  Color.fromRGBO(255, 51, 51, 0.8),
+                                  size: 25,
+                                ),
+                              ),
+                              ),
+                              ),
+                              PhysicalModel(
+                                elevation:2,
+                                color: Colors.transparent,
+                                shape: BoxShape.circle,
+                                child:
+                                CircleAvatar(
+                                  radius: 30,
+                                  backgroundColor:
+                                  Colors.white,
+                                  child: InkWell(
+                                    onTap: (){
 
+                                    },
+                                    child:Icon(
+                                    LineIcons.times,
+                                    color:
+                                    Color.fromRGBO(255, 51, 51, 0.8),
+                                    size: 25,
+                                  ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  )),
+              Positioned(
+                top: -40,
+                child:
+                Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 55,
+                      backgroundColor: Colors.white,
+                      child:
+                      CircleAvatar(
+                        radius: 40,
+                      child:Image.asset('assets/driver.png'),)
+                    ),
+                    Text('Prithvi',style: TextStyle(
+                      fontFamily: 'Arimo',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black54,
+                      fontSize: 16
+                    ))
+                  ],
+                ),
               ),
-            ],
+
+            ]
+            ),
+          ],
+        ));
+  }
+  Padding historyBox() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 6, bottom: 6),
+      child: Material(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+        elevation: 2.0,
+        child: SizedBox(
+          height: 150,
+          child: Padding(
+            padding: EdgeInsets.only(left: 20, right: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 100,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              '18:45',
+                              style: TextStyle(
+                                fontFamily: 'Arimo',
+                              ),
+                            ),
+                            Text('19:00',
+                                style: TextStyle(
+                                  fontFamily: 'Arimo',
+                                )),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Container(
+                        height: 90,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Icon(
+                              Icons.circle,
+                              color: Color.fromRGBO(255, 51, 51, 0.8),
+                              size: 10,
+                            ),
+                            Container(
+                              height: 60,
+                              child: VerticalDivider(
+                                color: Colors.black54,
+                                thickness: 2,
+                                indent: 2,
+                              ),
+                            ),
+                            Icon(
+                              Icons.arrow_drop_down,
+                              size: 20,
+                              color: Colors.black,
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Flexible(
+                          child: Container(
+                            height: 100,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  '59/Amman Street,Tirupur,641603',
+                                  style: TextStyle(
+                                      fontFamily: 'Arimo', color: Colors.grey),
+                                ),
+                                Text(
+                                  'Bannari Amman Institute of Technology,Sathy',
+                                  style: TextStyle(
+                                      fontFamily: 'Arimo', color: Colors.grey),
+                                ),
+                              ],
+                            ),
+                          )),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-        ]));
+        ),
+      ),
+    );
   }
 }
